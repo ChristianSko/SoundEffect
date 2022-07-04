0f894a41982ea8e80e340b1d3bac9a85e0a54cd2
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-04 10:55:35 +0200

    Initial Commit

---
+++ SoundEffect/SoundEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundEffectApp.swift
+//  SoundEffect
+//
+//  Created by Skorobogatow, Christian on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SoundEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
