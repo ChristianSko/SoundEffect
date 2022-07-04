3de63e9465523df3dc405695a93e9ca5de87cdf6
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-04 11:12:23 +0200

    feat(): add sounds to buttons with singleton

--- SoundEffect/ContentView.swift
+++ SoundEffect/ContentView.swift
@@ -6,11 +6,49 @@
 //
 
 import SwiftUI
+import AVKit
+
+
+class SoundManager {
+    
+    static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    enum SoundOption: String {
+        case tada
+        case badum
+    }
+    
+    func playSound(sound: SoundOption) {
+    
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sounds. \(error.localizedDescription)")
+        }
+    }
+}
+
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 40){
+            Button("Play Sound 1") {
+                SoundManager.instance.playSound(sound: .tada)
+                
+            }
+            
+            Button("Play Sound 2") {
+                SoundManager.instance.playSound(sound: .badum)
+            }
+        }
     }
 }
 
